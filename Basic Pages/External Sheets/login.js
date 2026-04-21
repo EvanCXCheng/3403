31785dbcb0b8f88cc2ef8c8fef019be1854b4e85
Mujtaba Al-Asadi <muji@Mujtabas-Air.modem>
2026-04-21 21:41:20 +0800

    Added email and password validation for account creation

--- Basic Pages/External Sheets/login.js
+++ Basic Pages/External Sheets/login.js
@@ -12,5 +12,41 @@ register.addEventListener('submit', (e) => {
   e.preventDefault();
   const username = document.getElementById('newname').value;
   const password = document.getElementById('newpassword').value;
+
+  if (!valid_email(username)) {
+    document.getElementById("email_error").textContent =
+    "Please enter a valid email address.";
+    return;
+  }
+  document.getElementById("email_error").textContent = "";
+
+  if (!valid_password(password)) {
+    document.getElementById("password_error").textContent =
+    "Password must contain uppercase, lowercase, number and be 8+ characters";
+    return;
+  }
+
+  document.getElementById("password_error").textContent = "";
   console.log(username, password);
 });
+
+function valid_password (password) {
+  if (password.length < 8) {
+    return false;
+  }
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+  if (!/[0-9]/.test(password)) {
+    return false;
+  }
+
+  return true;
+}
+
+function valid_email(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
